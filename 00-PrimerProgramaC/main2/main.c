e4c6fd92867cddcb1d689ca12d3b58a652207f4e
Pierre Richard Lauture <plauture@frba.utn.edu.ar>
2023-04-17 19:39:44 -0300

    Agregando otro proyecto main2.c en la carpeta 00PrimerPrograma

---
+++ 00-PrimerProgramaC/main2/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\"Hello world!!!\" -->> ");
+    printf(" es mi primer programa en C para poder subirlo en repositorio remoto\n");
+    return 0;
+}
