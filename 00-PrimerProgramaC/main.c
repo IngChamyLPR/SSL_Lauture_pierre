eeabce49228f27ca548b35b2da79512482b93378
Pierre Richard Lauture <plauture@frba.utn.edu.ar>
2023-04-17 20:03:23 -0300

    Actualizando el archivo de main hecho en VScode

--- 00-PrimerProgramaC/main.c
+++ 00-PrimerProgramaC/main.c
@@ -1,2 +1,8 @@
-cout << "Hola Mundo" << endl;
+int main()
+{
+    printf("\"Hello world!!!\" -->> ");
+    printf(" es mi primer programa en C para poder subirlo en repositorio remoto\n");
+    return 0;
+}
+
 
